77a1639de3f7d28e93a6a90709e0ca3881b8a245
Michal Marhan <marhanmichal@gmail.com>
2019-01-27 12:53:39 +0100

    Update dependencies
    Migrate to Gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp')
 var path = require('path')
 var del = require('del')
+var env = require('minimist')(process.argv.slice(2))
 var $ = require('gulp-load-plugins')({
   pattern: '*'
 })
 
 var environments = ['development', 'production']
-var environment = environments.indexOf($.util.env.t) > -1 ? $.util.env.t : 'development'
+var environment = environments.indexOf(env.t) > -1 ? env.t : 'development'
 var isProduction = (environment === 'production')
 var webpackConfig = require('./webpack.config.js')[environment]
 
-var port = $.util.env.p || 9001
+var port = env.p || 9001
 var src = 'web/app/'
 var dist = 'web/public/'
+
+gulp.task('clean', function (cb) {
+  del([dist]).then(function () {
+    cb()
+  })
+})
 
 gulp.task('scripts', function () {
   return gulp.src(webpackConfig.entry.app)
@@ -26,6 +33,14 @@ gulp.task('scripts', function () {
     .pipe($.connect.reload())
 })
 
+gulp.task('static', function (cb) {
+  return gulp.src(src + 'assets/**/*')
+    .pipe($.size({
+      title: 'static'
+    }))
+    .pipe(gulp.dest(dist + 'assets/'))
+})
+
 gulp.task('serve', function () {
   $.connect.server({
     root: dist,
@@ -37,34 +52,22 @@ gulp.task('serve', function () {
   })
 })
 
-gulp.task('static', function (cb) {
-  return gulp.src(src + 'assets/**/*')
-    .pipe($.size({
-      title: 'static'
-    }))
-    .pipe(gulp.dest(dist + 'assets/'))
-})
-
-gulp.task('watch', function () {
-  gulp.watch(src + 'assets/**/*', ['static'])
-  gulp.watch(src + '**/*.js', ['scripts'])
-  gulp.watch(src + '**/*.hbs', ['scripts'])
-  gulp.watch(src + '**/*.json', ['scripts'])
-  gulp.watch(src + '**/*.css', ['scripts'])
-})
-
-gulp.task('clean', function (cb) {
-  del([dist]).then(function () {
-    cb()
-  })
+gulp.task('watch', function watch () {
+  gulp.watch(src + 'assets/**/*', gulp.series('static'))
+  gulp.watch(src + '**/*.js', gulp.series('scripts'))
+  gulp.watch(src + '**/*.hbs', gulp.series('scripts'))
+  gulp.watch(src + '**/*.json', gulp.series('scripts'))
+  gulp.watch(src + '**/*.css', gulp.series('scripts'))
 })
 
 var defaultTasks = ['static', 'scripts']
 
 // waits until clean is finished then builds the project
-gulp.task('build', ['clean'], function () {
+gulp.task('build', gulp.series('clean'), function () {
   gulp.start(defaultTasks)
 })
 
 // by default build project and then watch files
-gulp.task('default', ['build', 'serve', 'watch'])
+
+gulp.task('default', gulp.parallel('build', 'serve', 'watch'), function () {
+})
