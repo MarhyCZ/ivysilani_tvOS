e227cf37b20a399094b5041e925af68eeb52523c
Jan Misar <misar.jan@gmail.com>
2019-09-08 14:05:49 +0200

    Use plain PNG channel images instead of broken LCR.

--- web/app/pages/live/index.js
+++ web/app/pages/live/index.js
@@ -19,24 +19,24 @@ const LivePage = ATV.Page.create({
         // Modifikace kanálů
         console.log(channels)
         channels.channel1.tintColor = '#ff0000'
-        channels.channel1.channelLogo = 'img/channels/ct1.lcr'
+        channels.channel1.channelLogo = 'img/channels/ct1.png'
 
         channels.channel2.tintColor = '#f7a400'
-        channels.channel2.channelLogo = 'img/channels/ct2.lcr'
+        channels.channel2.channelLogo = 'img/channels/ct2.png'
 
         if ('channel5' in channels) {
           channels.channel5.tintColor = '#ff6400'
-          channels.channel5.channelLogo = 'img/channels/ctd.lcr'
+          channels.channel5.channelLogo = 'img/channels/ctd.png'
         }
 
         channels.channel6.tintColor = 'gray'
-        channels.channel6.channelLogo = 'img/channels/ctart.lcr'
+        channels.channel6.channelLogo = 'img/channels/ctart.png'
 
         channels.channel24.tintColor = '#027dee'
-        channels.channel24.channelLogo = 'img/channels/ct24.lcr'
+        channels.channel24.channelLogo = 'img/channels/ct24.png'
 
         channels.channel4.tintColor = '#33cc00'
-        channels.channel4.channelLogo = 'img/channels/ctsport.lcr'
+        channels.channel4.channelLogo = 'img/channels/ctsport.png'
 
         Object.entries(channels).forEach(([key, value]) => {
           if (value.live.programme.channelTitle === '') {
